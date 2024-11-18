de4830449a706a95a916ebaa55a13192f7939463
GiovanniDadone <dadone.giovanni94@gmail.com>
2024-11-18 08:35:04 +0100

    added restaurants page part 1

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,28 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to MongoDB"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.get("/health", async (req: Request, res: Response) => {
-  res.send({message: "Health OK"});
+  res.send({ message: "Health OK" });
 });
 
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server is running on port 7000");
